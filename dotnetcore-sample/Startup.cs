1125f8cb0d6efcc88b80ce5cbbfaf9c4ec8d3b2e
jsamyo <jsamyo@yahoo.com>
2019-11-07 12:04:37 -0500

    Update Startup.cs

--- dotnetcore-sample/Startup.cs
+++ dotnetcore-sample/Startup.cs
@@ -18,7 +18,7 @@ namespace dotnetcore_sample
         {
             string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string appSettingsFilePath = Path.Combine(assemblyFolder,"appsettings.json");
-            string letTestDevOps = "";
+            string letTestDevOps = "Put some text here";
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
